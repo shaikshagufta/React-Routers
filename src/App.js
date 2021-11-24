68301e8dcdd3adb3d6df937aadf078df8c8bd633
shaikshagufta <shaikshagufta99@gmail.com>
2021-11-24 19:21:19 +0530

    initial comit-adding 3pages and hrefs using <a>

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+
+import React from 'react';
+import { BrowserRouter,Route,Switch } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header';
+import About from './pages/About';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+      </div>
+    <Switch>
+      <Route path= '/' component = {Home} exact />
+      <Route path= '/about' component = {About}/>
+      <Route path= '/profile' component = {Profile}/>
+    </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+const Header = () => {
+    return (
+        <>
+            <h1>React Router Tutorial</h1>
+            <ul className='nav'>
+                <li>
+                    <a href='/'>Home</a>
+                </li>
+                <li>
+                    <a href='/about'>About</a>
+                </li>
+                <li>
+                    <a href='profile'>Profile</a>
+                </li>
+            </ul>
+        </>
+    )
+}
+
+export default Header
