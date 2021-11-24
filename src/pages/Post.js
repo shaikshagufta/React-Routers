33e78b302c7678a5ecd05c8e47e56633ca567318
shaikshagufta <shaikshagufta99@gmail.com>
2021-11-24 21:58:33 +0530

    useLocation and useParams Hooks

--- src/pages/Post.js
+++ src/pages/Post.js
@@ -1,10 +1,18 @@
-import { useParams } from "react-router";
+import { useLocation, useParams } from "react-router";
 // fetchid id from url
 const Post = () => {
 
     const { id } = useParams();
+    //URL Search Params is an inbuilt API
+    const query = new URLSearchParams(useLocation().search)
 
-    return <h2>ID is = {id}</h2>
+    return (
+        <>
+            <h2>ID is = {id}</h2>
+            <h2>{query.get("first")}</h2>
+            <h2>{query.get("last")}</h2>
+        </>
+    )
 };
 
 export default Post;
