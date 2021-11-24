7b010cdcf34d4695d7ff0ecf641d99f3815e4341
shaikshagufta <shaikshagufta99@gmail.com>
2021-11-24 22:57:10 +0530

    Redirecting in ReactRouter using useHistory Hook

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,4 +1,15 @@
-const Profile = () => {
+import { useEffect } from "react";
+import { useHistory } from "react-router";
+
+const Profile = ({ login }) => {
+
+   const history = useHistory();
+    useEffect(() => {
+        if(!login) {
+            history.push('/about');
+        }   
+    }, [login, history]);
+
     return <h1>PROFILE PAGE</h1>;
 };
 
