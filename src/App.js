15e2ace937104a47f31961c81a696392de8dda19
shaikshagufta <shaikshagufta99@gmail.com>
2021-11-24 20:53:22 +0530

    page position

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 
-import React from 'react';
-import { BrowserRouter,Route,Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
 import About from './pages/About';
@@ -9,15 +8,15 @@ import Profile from './pages/Profile';
 
 function App() {
   return (
-    <BrowserRouter basename = "demo"> 
+    <BrowserRouter >
       <div className="App">
         <Header />
       </div>
-    <Switch>
-      <Route path= '/' component = {Home} exact />
-      <Route path= '/about' component = {About}/>
-      <Route path= '/profile' component = {Profile}/>
-    </Switch>
+      <Switch>
+        <Route path='/' component={Home} exact />
+        <Route path='/about' component={About} />
+        <Route path='/profile' component={Profile} />
+      </Switch>
     </BrowserRouter>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,14 @@
+import { useEffect } from "react"
+
 const Home = () => {
+
+    //to continue the other page from top insted of 
+    //its previous position the page was when we were on another page 
+    useEffect(() => {
+        window.scroll(0,0)
+    }, [])
+
+
     return (
         <div>
             HOME PAGE
